d98dadb5e56fb723c1c8e78e4d17d4f7c789db53
Mezoro <moaaz0raslan@gmail.com>
2024-05-09 20:28:20 -0400

    Separate token from user_info

--- routers/auth.py
+++ routers/auth.py
@@ -89,7 +89,6 @@ def verify_user(user_name, password, db):
         return False
     return True
 
-
 @router.post("/sign_up",status_code=201)
 async def create_user(cur: CreateUser, db: Session = Depends(get_db)):
     newUser = schemas.User()
@@ -104,8 +103,17 @@ async def create_user(cur: CreateUser, db: Session = Depends(get_db)):
     db.add(newUser)
     db.commit()
     token = create_access_token(newUser,timedelta(minutes=20))
+    user_info = {
+        "user_name": newUser.user_name,
+        "email": newUser.email,
+        "address": newUser.address,
+        "first_name": newUser.first_name,
+        "last_name": newUser.last_name,
+        "photo": newUser.photo,
+    }
     return {
-        'token':token
+        'token': token,
+        'user_info': user_info
     }
 
 
@@ -117,9 +125,19 @@ async def get_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Sessio
         raise HTTPException(status_code=404, detail="User not found !")
     user = db.query(schemas.User).filter(schemas.User.user_name == form_data.username).first()
     token = create_access_token(user, timedelta(minutes=20))
+    user_info = {
+        "user_name": user.user_name,
+        "email": user.email,
+        "address": user.address,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "photo": user.photo,
+    }
     return {
         'token': token,
+        'user_info':user_info
     }
 @router.get("/get_user")
 async def extract_info(user_info : dict = Depends(get_information_token)):
     return user_info
+
