a28a2021599bfa53c73cceca31b94422212cb7fc
Mohammad <mohmaddalil5@gmail.com>
2024-06-05 16:00:31 +0300

    adding product's APIs

--- controllers/productController.py
+++ controllers/productController.py
@@ -1,9 +1,61 @@
 from sqlalchemy.orm import Session
-
+from fastapi import status, HTTPException
 from models import schemas
 from models.instances import CreateProduct
 
 
 async def create_product(product: CreateProduct, db: Session):
     newProduct = schemas.Product()
-    # Mohammad is here
+    newProduct.title = product.title
+    newProduct.name = product.name
+    newProduct.photo = product.photo
+    newProduct.price = product.price
+    newProduct.description = product.description
+    newProduct.amount = product.amount
+    newProduct.category = product.category
+    newProduct.tags = product.tags
+    print(newProduct)
+    db.add(newProduct)
+    db.commit()
+    return {
+        "status": "success",
+        "data": {
+            "title": newProduct.title,
+            "name": newProduct.name,
+            "photo": newProduct.photo,
+            "price": newProduct.price,
+            "description": newProduct.description,
+            "amount": newProduct.amount,
+            "category": newProduct.category,
+            "tags": newProduct.tags,
+        }
+    }
+
+
+async def get_all_product_handler(db: Session, page: int, limit: int, search: str = ""):
+    products = db.query(schemas.Product).order_by(schemas.Product.id.asc()).filter(
+        schemas.Product.title.contains(search)).limit(limit).offset((page - 1) * limit).all()
+    return {
+        "status": "success",
+        "result": len(products),
+        "data": products
+    }
+
+async def getProduct_handler(product_id: int, db: Session):
+    product = db.query(schemas.Product).filter(schemas.Product.id == product_id).first()
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='invalid ID')
+    return {
+        "status": "success",
+        "data": product
+    }
+
+async def delete_product_handler(db: Session, product_id: int):
+    db_product = db.query(schemas.Product).filter(schemas.Product.id == product_id).first()
+    if not db_product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='invalid ID')
+    db.delete(db_product)
+    db.commit()
+    return {
+        "status": "sucess",
+    }
--- routers/productsRouter.py
+++ routers/productsRouter.py
@@ -1,17 +1,36 @@
 import sys
 sys.path.append("..")
-from fastapi import Depends, APIRouter, status
+from fastapi import Depends, APIRouter, status, Query
 from sqlalchemy.orm import Session
 from database import get_db
 
 from models.instances import CreateProduct
-from controllers.productController import create_product
+from controllers.productController import create_product, get_all_product_handler, getProduct_handler, delete_product_handler
 
 router = APIRouter(
     prefix = "/product",
     tags = ["product"]
 )
 
-@router.post("/addproduct", status_code = status.HTTP_201_CREATED)
+@router.post("/", status_code = status.HTTP_201_CREATED)
 async def create_product_handler(product: CreateProduct, db: Session = Depends(get_db)):
-    return create_product()
+    return await create_product(product, db)
+
+@router.get('/', status_code=status.HTTP_200_OK)
+async def get_all_product(
+    db: Session = Depends(get_db),
+    page: int = Query(1, ge=1, description="Page numebr"),
+    limit: int = Query(10, ge=1, le=100, description="Items per page"),
+    search: str | None = Query("", description="Search based title of products")
+):
+    return await get_all_product_handler(db, page, limit, search)
+
+
+@router.get('/{product_id}', status_code=status.HTTP_200_OK)
+async def getProduct(product_id: int, db: Session = Depends(get_db)):
+    return await getProduct_handler(product_id, db)
+
+@router.delete("/{product_id}", status_code=status.HTTP_200_OK)
+async def delete_product(product_id: int, db: Session = Depends(get_db)):
+    return await delete_product_handler(db, product_id)
+
