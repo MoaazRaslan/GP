fdf6903f7209db4e7ebbe877d3188d1c5a18743a
MohammadSobhi <129908365+MohammadSobhi@users.noreply.github.com>
2024-06-07 03:22:42 +0300

    added profile router and controller

---
+++ controllers/profileController.py
@@ -0,0 +1,61 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import session
+from controllers.authcontroller import get_information_token,create_access_token
+from models import schemas,instances
+
+def get_info_handler(db: session,token :str):
+    info = get_information_token(token)
+    user = db.query(schemas.User).filter(schemas.User.id == info.get("user_id")).first()
+    if user is None:
+        raise HTTPException(status_code=404,detail="User Not Found")
+    return {
+        'status': 'success',
+        'token': token,
+        'data': {
+            "username": user.username,
+            "email": user.email,
+            "address": user.address,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "photo": user.photo
+        }   
+    }
+
+def edit_info_handler(updated_info : instances.CreateUser,db: session,token :str):
+    info = get_information_token(token)
+    user = db.query(schemas.User).filter(schemas.User.id == info.get("user_id")).first()
+    if user is None:
+        raise HTTPException(status_code=404,detail="User Not Found")
+    user.first_name = updated_info.first_name
+    user.email = updated_info.email
+    user.last_name = updated_info.last_name
+    user.photo = updated_info.photo
+    user.address = updated_info.address
+    db.add(user)
+    db.commit()
+    token = create_access_token(user.id)
+    print(user.email)
+    return {
+        'status': 'success',
+        'token': token,
+        'data': {
+            "username": user.username,
+            "email": user.email,
+            "address": user.address,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "photo": user.photo
+        }   
+    }
+
+
+def delete_user_handler(db: session,token :str):
+    info = get_information_token(token)
+    user = db.query(schemas.User).filter(schemas.User.id == info.get("user_id")).first()
+    if user is None:
+        raise HTTPException(status_code=404,detail="User Not Found")
+    db.delete(user)
+    db.commit()
+    return {
+        'status': 'success', 
+    }
---
+++ routers/profileRouter.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter,status,Depends
+from sqlalchemy.orm import session
+from database import get_db
+from models.instances import CreateUser
+from controllers.authcontroller import oauth2_bearer
+from controllers.profileController import get_info_handler,edit_info_handler,delete_user_handler
+router = APIRouter(
+    prefix="/profile",
+    tags=["Profile"],
+    responses={404:{"user":"Not Found!"}}
+)
+
+@router.get("/myprofile",status_code=status.HTTP_200_OK)
+async def get_info(db: session = Depends(get_db),token :str = Depends(oauth2_bearer)):
+    return get_info_handler(db,token)
+
+
+@router.put("/editmyprofile",status_code=status.HTTP_200_OK)
+async def edit_info(updated_info: CreateUser,db: session = Depends(get_db),token :str = Depends(oauth2_bearer)):
+    return edit_info_handler(updated_info,db,token)
+
+@router.delete("/deleteuser",status_code=status.HTTP_200_OK)
+async def delete_user(db: session = Depends(get_db),token :str = Depends(oauth2_bearer)):
+    return delete_user_handler(db,token)
